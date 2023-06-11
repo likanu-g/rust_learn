734292207f56487d26860f1e5fd37f148e63190f
likanu-g <1452930742@qq.com>
2023-06-11 18:58:19 +0800

    聚合配置变量

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,12 +3,21 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     //dbg!(args);
-    let query = &args[1];
-    let file_path = &args[2];
 
-    /*     println!("Searching for {}", query);
-    println!("In file {}", file_path); */
+    let config = parse_config(&args);
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
     println!("With text:\n {}", contents)
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].to_string();
+    let file_path = args[2].to_string();
+    Config { query, file_path }
 }
